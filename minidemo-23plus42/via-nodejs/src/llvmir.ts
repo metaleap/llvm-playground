4478f289988db962cebbcab499f5d4c90a31a653
metaleap <metaleap@users.noreply.github.com>
2018-10-03 20:33:52 +0200

    [autopush] «I'd rather push my Harley than ride a rice burner.»

--- minidemo-23plus42/via-nodejs/src/llvmir.ts
+++ minidemo-23plus42/via-nodejs/src/llvmir.ts
@@ -8,50 +8,50 @@
 */
 
 export enum LlvmType {
-	i32 = "i32"
+    i32 = "i32"
 }
 
 interface LlvmSyn {
-	srcIR(): string
+    srcIR(): string
 }
 
 export class LlvmModule implements LlvmSyn {
-	constructor(
-		readonly name: string,
-		readonly funcs: LlvmFunc[] = [],
-	) { }
+    constructor(
+        readonly name: string,
+        readonly funcs: LlvmFunc[] = [],
+    ) { }
 
-	srcIR = () =>
-		[`; ModuleID = '${this.name}'`,
-		`source_filename = "${this.name}"`].concat(
-			this.funcs.map(fn => fn.srcIR()),
-		).join("\n")
+    srcIR = () =>
+        [`; ModuleID = '${this.name}'`,
+        `source_filename = "${this.name}"`].concat(
+            this.funcs.map(fn => fn.srcIR()),
+        ).join("\n")
 }
 
 export class LlvmFunc implements LlvmSyn {
-	constructor(
-		readonly name: string,
-		readonly type: LlvmType,
-		readonly blocks: LlvmBlock[] = [],
-	) { }
+    constructor(
+        readonly name: string,
+        readonly type: LlvmType,
+        readonly blocks: LlvmBlock[] = [],
+    ) { }
 
-	srcIR = () =>
-		[`define ${this.type} @${this.name}() {`].concat(
-			this.blocks.map(block => block.srcIR()),
-			["}"],
-		).join("\n")
+    srcIR = () =>
+        [`define ${this.type} @${this.name}() {`].concat(
+            this.blocks.map(block => block.srcIR()),
+            ["}"],
+        ).join("\n")
 }
 
 export class LlvmBlock implements LlvmSyn {
-	constructor(
-		readonly name: string,
-		readonly instrs: LlvmInstr[] = [],
-	) { }
+    constructor(
+        readonly name: string,
+        readonly instrs: LlvmInstr[] = [],
+    ) { }
 
-	srcIR = () =>
-		[`${this.name}:`].concat(
-			this.instrs.map(instr => "\t" + instr.srcIR()),
-		).join("\n")
+    srcIR = () =>
+        [`${this.name}:`].concat(
+            this.instrs.map(instr => "\t" + instr.srcIR()),
+        ).join("\n")
 }
 
 interface LlvmInstr extends LlvmSyn { }
@@ -59,55 +59,55 @@ interface LlvmInstr extends LlvmSyn { }
 var next: number = -1
 
 export class LlvmInstrAlloca implements LlvmInstr {
-	readonly name: number
-	constructor(
-		readonly type: LlvmType,
-	) { this.name = ++next }
+    readonly name: number
+    constructor(
+        readonly type: LlvmType,
+    ) { this.name = ++next }
 
-	srcIR = () =>
-		`%${this.name} = alloca ${this.type}`
+    srcIR = () =>
+        `%${this.name} = alloca ${this.type}`
 }
 
 export class LlvmInstrStore implements LlvmInstr {
-	constructor(
-		readonly type: LlvmType,
-		readonly val: any,
-		readonly name: number,
-	) { }
+    constructor(
+        readonly type: LlvmType,
+        readonly val: any,
+        readonly name: number,
+    ) { }
 
-	srcIR = () =>
-		`store ${this.type} ${this.val}, ${this.type}* %${this.name}`
+    srcIR = () =>
+        `store ${this.type} ${this.val}, ${this.type}* %${this.name}`
 }
 
 export class LlvmInstrLoad implements LlvmInstr {
-	readonly name: number
-	constructor(
-		readonly type: LlvmType,
-		readonly fromName: number,
-	) { this.name = ++next }
+    readonly name: number
+    constructor(
+        readonly type: LlvmType,
+        readonly fromName: number,
+    ) { this.name = ++next }
 
-	srcIR = () =>
-		`%${this.name} = load ${this.type}, ${this.type}* %${this.fromName}`
+    srcIR = () =>
+        `%${this.name} = load ${this.type}, ${this.type}* %${this.fromName}`
 }
 
 export class LlvmInstrAdd implements LlvmInstr {
-	readonly name: number
-	constructor(
-		readonly type: LlvmType,
-		readonly nameL: number,
-		readonly nameR: number,
-	) { this.name = ++next }
+    readonly name: number
+    constructor(
+        readonly type: LlvmType,
+        readonly nameL: number,
+        readonly nameR: number,
+    ) { this.name = ++next }
 
-	srcIR = () =>
-		`%${this.name} = add ${this.type} %${this.nameL}, %${this.nameR}`
+    srcIR = () =>
+        `%${this.name} = add ${this.type} %${this.nameL}, %${this.nameR}`
 }
 
 export class LlvmInstrRet implements LlvmInstr {
-	constructor(
-		readonly type: LlvmType,
-		readonly name: number,
-	) { }
+    constructor(
+        readonly type: LlvmType,
+        readonly name: number,
+    ) { }
 
-	srcIR = () =>
-		`ret ${this.type} %${this.name}`
+    srcIR = () =>
+        `ret ${this.type} %${this.name}`
 }

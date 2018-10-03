5f743d38bca3393f0f6d86b99d70e91b12d88147
metaleap <metaleap@users.noreply.github.com>
2018-10-04 00:46:18 +0200

    [autopush] «We are the people our parents warned us about.»

--- tut-kaleidoscope/01-lexer/out/tut-kaleidoscope/01-lexer/src/main.js
+++ tut-kaleidoscope/01-lexer/out/tut-kaleidoscope/01-lexer/src/main.js
@@ -9,7 +9,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const llvmir = __importStar(require("../../../node-llvm-ir/src/llvmir"));
 const lex = __importStar(require("./lex"));
-const tmp = llvmir.Type.i32;
 const src1 = `
 def fib(x)
   if x < 3 then
@@ -26,6 +25,7 @@ extern atan2(arg1 arg2);
 
 atan2(sin(.4), cos(42))
 `;
-const src = (tmp !== llvmir.Type.i32) ? src1 : src2;
-for (const tok of lex.tokens(src))
+const srcs = [src1, src2, llvmir.Type.i32];
+const idx = (Math.random() * (srcs.length - 1)) | 0;
+for (const tok of lex.tokens(srcs[idx]))
     console.log(tok.raw);
--- tut-kaleidoscope/01-lexer/src/main.ts
+++ tut-kaleidoscope/01-lexer/src/main.ts
@@ -2,8 +2,6 @@ import * as llvmir from "../../../node-llvm-ir/src/llvmir"
 
 import * as lex from './lex'
 
-const tmp = llvmir.Type.i32
-
 const src1 = `
 def fib(x)
   if x < 3 then
@@ -22,5 +20,6 @@ extern atan2(arg1 arg2);
 atan2(sin(.4), cos(42))
 `
 
-const src = (tmp !== llvmir.Type.i32) ? src1 : src2
-for (const tok of lex.tokens(src)) console.log(tok.raw)
+const srcs = [src1, src2, llvmir.Type.i32 /* last item is never used, just referenced so we dont have to ditch the import which would wreck the ./out/* dir structure (crikey, TS!) */]
+const idx = (Math.random() * (srcs.length - 1)) | 0 // int
+for (const tok of lex.tokens(srcs[idx])) console.log(tok.raw)
